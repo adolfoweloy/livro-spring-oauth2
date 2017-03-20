86daacb8b1019438b5e048a1c4034870acc95d0d
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-03-20 00:04:57 -0300

    Código de exemplo para o capitulo sobre o grant type client credentials

---
+++ exemplo-6/bookserver/src/main/java/br/com/casadocodigo/configuracao/web/ConfiguracaoMvc.java
@@ -0,0 +1,36 @@
+package br.com.casadocodigo.configuracao.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ConfiguracaoMvc extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver resolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        resolver.setCacheable(false);
+
+        templateEngine.setTemplateResolver(resolver);
+        templateEngine.addDialect(new SpringSecurityDialect());
+        return templateEngine;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/**");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
