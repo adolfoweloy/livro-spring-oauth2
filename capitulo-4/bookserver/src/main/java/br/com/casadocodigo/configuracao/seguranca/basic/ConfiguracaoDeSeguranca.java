9ccadff060915399b842cf77d1b055f7f9c83aa5
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-05-07 20:28:10 -0300

    correção de imports

--- capitulo-4/bookserver/src/main/java/br/com/casadocodigo/configuracao/seguranca/basic/ConfiguracaoDeSeguranca.java
+++ capitulo-4/bookserver/src/main/java/br/com/casadocodigo/configuracao/seguranca/basic/ConfiguracaoDeSeguranca.java
@@ -1,10 +1,6 @@
 package br.com.casadocodigo.configuracao.seguranca.basic;
 
-import br.com.casadocodigo.configuracao.seguranca.DadosDoUsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
