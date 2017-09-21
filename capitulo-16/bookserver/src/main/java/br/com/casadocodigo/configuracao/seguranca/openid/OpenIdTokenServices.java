4805a21518a7d33fb2a4c1adb6046b271046462f
Adolfo Eloy <adolfo.eloy@gmail.com>
2017-09-20 21:29:34 -0300

    Estrutura basica para a classe que persiste os dados do token no capitulo 16 (codigo de exemplo)

--- capitulo-16/bookserver/src/main/java/br/com/casadocodigo/configuracao/seguranca/openid/OpenIdTokenServices.java
+++ capitulo-16/bookserver/src/main/java/br/com/casadocodigo/configuracao/seguranca/openid/OpenIdTokenServices.java
@@ -1,43 +1,16 @@
 package br.com.casadocodigo.configuracao.seguranca.openid;
 
-import br.com.casadocodigo.usuarios.AutenticacaoOpenid;
-import br.com.casadocodigo.usuarios.autenticacao.UsuarioAutenticado;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.ClientTokenServices;
+import javax.transaction.Transactional;
+
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-
 @Service
 @Transactional
-public class OpenIdTokenServices implements ClientTokenServices {
+public class OpenIdTokenServices {
 
-    @Override
-    public OAuth2AccessToken getAccessToken(OAuth2ProtectedResourceDetails resource,
-        Authentication authentication) {
-        if (authentication == null) return null;
-
-        UsuarioAutenticado userDetails = (UsuarioAutenticado) authentication.getPrincipal();
-        AutenticacaoOpenid autenticacaoOpenId = userDetails.getAutenticacaoOpenId();
-
-        if (autenticacaoOpenId.expirou()) {
-            return null;
-        }
-
-        return userDetails.getToken();
-    }
-
-    @Override
-    public void saveAccessToken(OAuth2ProtectedResourceDetails resource,
-        Authentication authentication, OAuth2AccessToken accessToken) {
+    public void saveAccessToken(OAuth2AccessToken accessToken) {
 
     }
 
-    @Override
-    public void removeAccessToken(OAuth2ProtectedResourceDetails resource,
-        Authentication authentication) {
-        throw new UnsupportedOperationException("Operação não suportada");
-    }
 }
